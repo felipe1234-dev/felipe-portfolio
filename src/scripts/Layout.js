7761e52a1b00f2a0fa805fceee5377a3fa2c580a
felipe1234-dev <felipejean2002@gmail.com>
2021-10-01 11:10:37 -0300

    Criar o componente principal: Layout
    Layout já vem com os componentes internos dispostos que serão criados logo em seguida.

---
+++ src/scripts/Layout.js
@@ -0,0 +1,21 @@
+import Navbar from "./navbar/Navbar";
+import Header from "./header/Header";
+import About from "./about/About";
+import Services from "./services/Services";
+import Resume from "./resume/Resume";
+import Projects from "./projects/Projects";
+
+function Layout() {
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <About />
+            <Services />
+            <Resume />
+            <Projects />
+        </>
+    );
+}
+
+export default Layout;
