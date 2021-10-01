7151f539c43663778f2691d4b9f4247b4c3b5afe
felipe1234-dev <felipejean2002@gmail.com>
2021-10-01 11:28:31 -0300

    Criar Resume
    O botão ainda terá a funcionalidade de baixar o currículo.

---
+++ src/scripts/resume/Resume.js
@@ -0,0 +1,20 @@
+function Resume() {
+    return (
+        <section className="section" id="resume">
+            <div className="section-heading">
+                <h3 className="title is-2">
+                    Resume
+                </h3>
+                <h4 className="subtitle is-5">
+                    More about my past
+                </h4>
+                <a href="#" className="button is-link is-medium">
+                    <span className="icon">
+                        <i className="fas fa-file-alt"></i>
+                    </span>
+                    <span>Download My Resume</span>
+                </a>
+            </div>
+        </section>
+    );
+}
