25bf1a46687a86c8b99408eaebdfa8527611d33c
felipe1234-dev <felipejean2002@gmail.com>
2021-10-01 11:32:37 -0300

    Criando About e Title

---
+++ src/scripts/about/About.js
@@ -0,0 +1,14 @@
+import Title from "./Title";
+
+function About() {
+    return (
+        <section 
+            id="about" 
+            className="section"
+        >
+            <Title />
+        </section>
+    );
+}
+
+export default About;
