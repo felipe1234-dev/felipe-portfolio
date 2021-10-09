1a8659a31ce625d05a56242d8f4b026568320b84
felipe1234-dev <felipejean2002@gmail.com>
2021-10-09 20:22:28 -0300

    Corrigindo erro com menu && " is-active"
    - menu && " is-active" vai retornar a string "false" quando for falso, portanto, foi substituída por menu ? " is-active" : "" que retorna nada quando for falsa.
    - corrigindo o import do objeto sections já que o arquivo com função de exportador geral foi criado (caminho: ./src/json/index).
    - colocando o efeito de scroll da biblioteca react-scrolling-effects nas âncoras

--- src/scripts/navbar/Anchors.js
+++ src/scripts/navbar/Anchors.js
@@ -1,12 +1,16 @@
-import { sections } from "src/json/sections";
+import { sections } from "src/json";
+import { scroll, easeOutBounce } from "react-scrolling-effects";
 
 const Anchors = ({ menu }) => (
-    <div className={`navbar-menu${menu && " is-active"}`}>
+    <div className={`navbar-menu${menu ? " is-active" : ""}`}>
         <div className="navbar-end">
             {sections.map(anchor => (
                 <a
                     className="navbar-item" 
-                    href={`#${anchor.value}`}
+                    onClick={() => scroll(`#${anchor.value}`, {
+                        duration: 1000,
+                        effect: easeOutBounce
+                    })}
                 >
                     <span className="icon">
                         <i className={anchor.icon}></i>
