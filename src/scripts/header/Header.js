5f5704eaef7321a3aac4454127130dca5b42a563
felipe1234-dev <felipejean2002@gmail.com>
2021-10-09 20:37:52 -0300

    Fazer dar a impressão de que os títulos 1, 2 e 3 são digitados em sequência
    A biblioteca react-typing-effect faz um efeito de o texto ser digitado na tela, nesse componente (Header), há três "títulos" (texto, título e subtítulo, na verdade) e o objetivo foi fazê-los parecer estar sendo digitados em sequência. Para isso, usei setTimeout()s para esconder e revelar os cursores que a biblioteca faz sincronicamente um após o outro.

--- src/scripts/header/Header.js
+++ src/scripts/header/Header.js
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactTypingEffect from "react-typing-effect";
 
 const Typewriter = ReactTypingEffect;
 
+const TITLE_1_DELAY = 2000;
+const TITLE_2_DELAY = 5500;
+const TITLE_3_DELAY = 9000;
+
 function Header() {
+
+    useEffect(() => {
+        const $ = query => document.querySelector(query);
+
+        $("#cursor-1").style.display = "block";
+        $("#cursor-2").style.display = "none";
+        $("#cursor-3").style.display = "none";
+
+        setTimeout(() => {
+            $("#cursor-1").style.display = "none";
+            $("#cursor-2").style.display = "block";
+        }, TITLE_2_DELAY);
+        setTimeout(() => {
+            $("#cursor-2").style.display = "none";
+            $("#cursor-3").style.display = "block";
+        }, TITLE_3_DELAY);
+    }, []);
+
     const props = {
-        title: {
-            text: "Olá! Eu sou Felipe Alves Desenvolvedor Web Full Stack", 
+        title1: {
+            text: "Olá! Eu sou", 
             speed: 250,
-            eraseDelay: 1500,
-            typingDelay: 500,
-            cursorRenderer: cursor => <span>{cursor}</span>,
-            displayTextRenderer: () => (
-                <div className="container">
-                    Olá! Eu sou
-                    <h1 className="title is-1">
-                        Felipe Alves
-                    </h1>
-                    <h2 className="subtitle is-3">
-                        Desenvolvedor Web Full Stack
-                    </h2>
-                </div>
+            eraseDelay: 3600000,
+            typingDelay: TITLE_1_DELAY,
+            cursorRenderer: cursor => (
+                <span id="cursor-1">
+                    {cursor}
+                </span>
+            )
+        },
+        title2: {
+            text: "Felipe Alves", 
+            speed: 250,
+            eraseDelay: 3600000,
+            typingDelay: TITLE_2_DELAY,
+            cursorRenderer: cursor => (
+                <h1 className="title is-1" id="cursor-2">
+                    {cursor}
+                </h1>
+            ),
+            displayTextRenderer: text => (
+                <h1 className="title is-1">
+                    {text}
+                </h1>
+            )
+        },
+        title3: {
+            text: "Desenvolvedor Web Full Stack",
+            speed: 250,
+            eraseDelay: 3600000,
+            typingDelay: TITLE_3_DELAY,
+            cursorRenderer: cursor => (
+                <h2 className="subtitle is-3" id="cursor-3">
+                    {cursor}
+                </h2>
+            ),
+            displayTextRenderer: text => (
+                <h2 className="subtitle is-3">
+                    {text}
+                </h2>
             )
         }
     }
@@ -30,7 +77,13 @@ function Header() {
             className="hero is-link is-fullheight is-fullheight-with-navbar"
         >
             <div className="hero-body">
-                <Typewriter { ...props.title } />
+                <div className="container">
+                    <Typewriter { ...props.title1 } />
+                    <br />
+                    <Typewriter { ...props.title2 } />
+                    <br />
+                    <Typewriter { ...props.title3 } />
+                </div>
             </div>
         </header>
     );
