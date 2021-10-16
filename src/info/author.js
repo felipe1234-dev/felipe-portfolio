29f327a02fa876a408e43b6ceec8033a46081737
felipe1234-dev <felipejean2002@gmail.com>
2021-10-16 13:36:05 -0300

    Criando o objeto "author" que, basicamente, terá algumas informações sobre mim

---
+++ src/info/author.js
@@ -0,0 +1,17 @@
+const author = {
+    name: "Felipe Alves",
+    links: [
+        {
+            label: "linkedIn",
+            icon: "fab fa-linkedin",
+            href: "https://www.linkedin.com/in/felipe-j-alves/"
+        },
+        {
+            label: "github",
+            icon: "fab fa-github-alt",
+            href: "https://github.com/felipe1234-dev/"
+        }
+    ]
+}
+
+export default author;
---
+++ src/info/index.js
@@ -0,0 +1,11 @@
+import author from "./author";
+import projects from "./projects";
+import sections from "./sections";
+import services from "./services";
+
+export {
+    author,
+    projects,
+    sections,
+    services
+};
