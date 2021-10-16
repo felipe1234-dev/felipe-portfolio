ff311c5b850e255f0df1068b5b21d2589dc0133c
felipe1234-dev <felipejean2002@gmail.com>
2021-10-16 13:52:35 -0300

    src/json -> src/info e href como identificador no SCSS
    Adaptando à mudança de nome da pasta (json -> info) e colocando o atributo href como identificador no SCSS para colocar mais estilos.

--- src/scripts/navbar/Anchors.js
+++ src/scripts/navbar/Anchors.js
@@ -1,21 +1,25 @@
-import { sections } from "src/json";
+import { sections } from "src/info";
 import { scroll, easeOutBounce } from "react-scrolling-effects";
 
 const Anchors = ({ menu }) => (
     <div className={`navbar-menu${menu ? " is-active" : ""}`}>
         <div className="navbar-end">
-            {sections.map(anchor => (
+            {sections.map(({ label, value, icon }) => (
                 <a
-                    className="navbar-item" 
-                    onClick={() => scroll(`#${anchor.value}`, {
-                        duration: 1000,
-                        effect: easeOutBounce
-                    })}
+                    className="navbar-item"
+                    href={`#${value}`}
+                    onClick={event => {
+                        event.preventDefault();
+                        scroll(`#${value}`, {
+                            duration: 1000,
+                            effect: easeOutBounce
+                        });
+                    }}
                 >
                     <span className="icon">
-                        <i className={anchor.icon}></i>
+                        <i className={icon}></i>
                     </span>
-                    <span>{anchor.label}</span>
+                    <span>{label}</span>
                 </a>
             ))}
         </div>
