ef886dcce11922ee37007b4a7b659f9da8ab8f49
felipe1234-dev <felipejean2002@gmail.com>
2021-10-21 00:07:28 -0300

    Update projects.js

--- src/info/projects.js
+++ src/info/projects.js
@@ -1,24 +1,29 @@
-import CoronavirusMapImg from "src/media/coronavirus-map/mobile.jpg";
+import CoronavirusMapImg from "src/media/coronavirus-map/demo.gif";
+import WebTranslatorsImg from "src/media/web-translators/demo_landing.jpg";
 
 const projects = {
-    row1: [
-        {
-            image: CoronavirusMapImg,
-            title: "Sistema de Monitoramento da COVID",
-            description: "Um aplicativo de mapeamento que usa uma API contendo estatísticas recentes do Coronavirus e mapeia os locais e o impacto que cada país está enfrentando.",
-            preview: "https://coronavirus-map-dashboard.herokuapp.com/",
-            source: "https://github.com/felipe1234-dev/coronavirus-map",
-            techs: [ "React", "CSS", "HTML", "JavaScript", "RSuite", "Disease.h", "Mapbox", "React Leaflet", "Axios" ]
-        },
-        {
-            image: CoronavirusMapImg,
-            title: "Site Tradutores da Web",
-            description: "Web Translators é um projeto em andamento, é um lugar para traduzirmos artigos e vídeos do idioma que estamos aprendendo para o idioma que já falamos, a fim de ajudar os aspirantes a falantes de idiomas.",
-            preview: "https://coronavirus-map-dashboard.herokuapp.com/",
-            source: "https://github.com/felipe1234-dev/coronavirus-map",
-            techs: [ "React", "SASS", "HTML", "JavaSript", "NodeJs", "RSuite", "SQLite" ]
-        }
-    ]
+    title: "💻 Projetos",
+    subtitle: "Meus últimos trabalhos",
+    list: {
+        row1: [
+            {
+                image: CoronavirusMapImg,
+                title: "Sistema de Monitoramento da COVID",
+                description: "Um aplicativo de mapeamento que usa uma API contendo estatísticas recentes do Coronavirus e mapeia os locais e o impacto que cada país está enfrentando.",
+                preview: "https://coronavirus-map-dashboard.herokuapp.com/",
+                source: "https://github.com/felipe1234-dev/coronavirus-map",
+                techs: [ "React", "CSS", "HTML", "JavaScript" ]
+            },
+            {
+                image: WebTranslatorsImg,
+                title: "Site Tradutores da Web",
+                description: "Uma plataforma colaborativa onde traduzimos páginas da internet do idioma que estamos aprendendo para o idioma que já falamos.",
+                preview: "https://web-translators.herokuapp.com/",
+                source: "https://github.com/felipe1234-dev/tradutores-web",
+                techs: [ "React", "SASS", "HTML", "JavaScript", "NodeJS", "SQLite" ]
+            }
+        ]
+    }
 }
 
 export default projects;
