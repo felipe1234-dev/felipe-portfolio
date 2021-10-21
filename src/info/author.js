a71552a0a99ae6158eb3ce5baaf3f0b9b2131a0c
felipe1234-dev <felipejean2002@gmail.com>
2021-10-21 00:08:31 -0300

    Adicionando sobre e minha foto

--- src/info/author.js
+++ src/info/author.js
@@ -1,5 +1,29 @@
+import photo from "src/media/photo.jpg";
+
 const author = {
     name: "Felipe Alves",
+    about: `
+        <p>
+            Olá! 
+            <br/>
+            Tenho 19 anos e estudo programação desde os 17, adoro tanto design quanto backend 
+            <br/>
+            Estou cursando Análise e Desenvolvimento de Sistemas na Uninter, sou bilíngue (Português e Inglês), 
+            mas tenho bom domínio na língua escrita de Francês e Espanhol. 
+            <br/>
+            Almejando ser pós-graduado em Segurança da Informação e Redes, tenho qualificações principalmente na área
+            de Programação Web, com cursos e estudo autodidata e na freeCodeCamp em React, NodeJs, MySQL, HTML, CSS e Javascript. 
+            <br/>
+            Tenho um bom conhecimentos em algumas outras linguagens como: PHP (OOP) e Java.
+            <br/>
+            Antes de embarcar na programação, meus principais afazeres eram estudar idiomas e astronomia :)
+            <br/>
+            Tais assuntos que ainda permanecem em meu coração.
+            <br/>
+            Procurando uma oportunidade para trabalhar e de me melhorar.
+        </p>
+    `,
+    photo: photo,
     links: [
         {
             label: "linkedIn",
