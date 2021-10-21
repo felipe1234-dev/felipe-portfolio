d8274c3b4508b2513c2c5d38db04637d5345ef73
felipe1234-dev <felipejean2002@gmail.com>
2021-10-21 00:07:19 -0300

    Colocar skills no lugar de services

--- src/info/sections.js
+++ src/info/sections.js
@@ -1,10 +1,9 @@
 const sections = [
     { label: "Início",      value: "home",      icon: "fas fa-home"     },
     { label: "Sobre mim",   value: "about",     icon: "fas fa-info"     },
-    { label: "Serviços",    value: "services",  icon: "fas fa-bars"     },
+    { label: "Skills",      value: "skills",    icon: "fas fa-bars"     },
     { label: "Currículo",   value: "resume",    icon: "fas fa-file-alt" },
-    { label: "Projetos",    value: "projects",  icon: "fas fa-th-list"  },
-    { label: "Contato",     value: "contact",   icon: "fas fa-envelope" }
+    { label: "Projetos",    value: "projects",  icon: "fas fa-th-list"  }
 ];
 
 export default sections;
