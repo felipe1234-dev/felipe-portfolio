fdd1a997f184bede5e7f272ddc2b0de529fbdf30
felipe1234-dev <felipejean2002@gmail.com>
2021-10-20 23:52:11 -0300

    Update Card.js

--- src/scripts/projects/Card.js
+++ src/scripts/projects/Card.js
@@ -1,29 +1,62 @@
-function Card({ title, image, techs, preview, source:srcCode }) {
+import "src/styles/Card.scss";
+
+function Card({ title, image, description: desc, techs, preview, source:github }) {
+    const truncate = (str, n) => {
+        return str.length > n ? str.substr(0, n - 1) + "(...)" : str;
+    }
+
     return (
         <div className="card">
+
             <header className="card-header">
-                <p className="card-header-title">
-                    <span>{title}</span>
-                    <span className="is-pulled-right">
-                        {techs.map(tech => (
-                            <span className="tag" tech={tech}>{tech}</span>
-                        ))}
-                    </span>
-                </p>
+                <div className="browser-header">
+                    
+                    <span className="fake-button" data-close=""></span>
+                    <span className="fake-button" data-minify=""></span>
+                    <span className="fake-button" data-expand=""></span>
+                    
+                </div>
             </header>
 
-            <div className="card-content">
+            <div className="card-image">
                 <figure className="image">
-                    <img src={image} alt={title} />
+                    <img 
+                        className="has-ratio" 
+                        src={image} 
+                        alt={title} 
+                    />
                 </figure>
+                <div className="overlay">
+                    <div className="content">
+                        <h3 className="title is-6"> 
+                            {title}
+                        </h3>
+                        <p>{truncate(desc, 200)}</p>
+                        {techs.map(tech => (
+                            <span className="tag" tech={tech}>
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
+                </div>
             </div>
             
             <footer className="card-footer">
-                <a href={preview} className="card-footer-item">
+                <a 
+                    href={preview} 
+                    className="card-footer-item button is-normal"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     Preview
                 </a>
-                <a href={srcCode} className="card-footer-item">
-                    Source Code
+                <a 
+                    href={github} 
+                    className="card-footer-item button is-normal"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Github
                 </a>
             </footer>
         </div>
