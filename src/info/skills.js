f6577570d3864d48653dd93cf740c44634eb8176
felipe1234-dev <felipejean2002@gmail.com>
2021-10-21 00:07:00 -0300

    Colocar skills no lugar

---
+++ src/info/skills.js
@@ -0,0 +1,36 @@
+const skills = {
+    title: "💪 Skills",
+    subtitle: "",
+    list: {
+        "Front end": {
+            row1: [
+                { name: "HTML", percent: 100 },
+                { name: "CSS", percent: 90  }
+            ], 
+            row2: [
+                { name: "JavaScript", percent: 90 },
+                { name: "React", percent: 95 }
+            ],
+            row3: [
+                { name: "SASS", percent: 100  }
+            ]
+        },
+        "Back end": {
+            row1: [
+                { name: "NodeJS", percent: 80 },
+                { name: "Express", percent: 80 }
+            ],
+            row2: [
+                { name: "PHP", percent: 60 },
+                { name: "Java", percent: 30 }
+            ]
+        },
+        "Banco de dados": {
+            row1: [
+                { name: "SQL", percent: 89 }
+            ]
+        }
+    }
+};
+
+export default skills;
