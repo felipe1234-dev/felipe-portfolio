073c0e922b03545d407920746c986e0a3ad7d209
felipe1234-dev <felipejean2002@gmail.com>
2021-10-21 00:06:00 -0300

    Update About.js

--- src/scripts/about/About.js
+++ src/scripts/about/About.js
@@ -1,12 +1,39 @@
-import Title from "./Title";
+import React, { useEffect, useContext } from "react";
+import { author, about } from "src/info";
+import { NavContext } from "../contexts";
 
-function About() {
+function About({ isVisible }) {
+    const [setActiveItem] = useContext(NavContext);
+
+    useEffect(() => isVisible ? setActiveItem("about") : null, [isVisible]);
+
     return (
         <section 
             id="about" 
             className="section"
         >
-            <Title />
+            <div className="section-heading">
+                <h3 className="title is-2">
+                    {about.title}
+                    <b className="dot">
+                        .
+                    </b>
+                </h3>
+                <h4 className="subtitle is-5">
+                    {about.subtitle}
+                </h4>
+                <div 
+                    className="container content" 
+                    dangerouslySetInnerHTML={{ __html: author.about }} 
+                />
+                <figure className="image is-128x128">
+                    <img 
+                        className="is-rounded" 
+                        src={author.photo}
+                        alt={author.name}
+                    />
+                </figure>
+            </div>
         </section>
     );
 }
